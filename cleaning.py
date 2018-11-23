18cd290a0ca300f8ea44a6ba32342dcf6d770db4
kingarpharazon <kingarpharazon@live.com.mx>
2018-11-23 01:03:49 -0600

    Implement time spliting method
    This method splits the column 'HORA' in two columsn 'HORA_INICIO' and
    'HORA_FIN'.

--- cleaning.py
+++ cleaning.py
@@ -1,14 +1,24 @@
 import sys
 import getopt
+import pandas as pd
+import numpy as np
 
+def split_crime_time(filename):
+    df = pd.read_csv(filename)
+
+    df['HORA_INICIO'], df['HORA_FIN'] = df['HORA'].str.split(' A ', 1).str
+
+    df['HORA_FIN'] = np.where(pd.isna(df['HORA_FIN']), df['HORA_INICIO'], df['HORA_FIN'])
+
+    df.to_csv('output.csv')
 
 def main(argv):
     filename = 'Delitos.csv'
 
     try:
-        opts, args = getopt.getopt(argv, "hf:")
+        opts, args = getopt.getopt(argv, "hf:t")
     except:
-        print('cleaning.py -f <filename>')
+        print('cleaning.py -f <filename> -t')
         sys.exit(1)
 
     for opt, arg in opts:
@@ -17,9 +27,10 @@ def main(argv):
             sys.exit(1)
         elif opt == '-f':
             filename = arg
+        elif opt == '-t':
+            split_crime_time(filename)
 
-
-    print(filename)
+    print('Finished')
 
 
 if __name__ == '__main__':
